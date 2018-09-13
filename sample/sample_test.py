9b55894302812e66ff6f834598f2b067b1fcc398
Kern <gf4t47@gmail.com>
2018-09-13 09:10:27 -0700

    comments updating.

--- sample/sample_test.py
+++ sample/sample_test.py
@@ -5,16 +5,16 @@ from src.index import execute
 
 
 @pytest.mark.parametrize("asm_file, expected", [
-    ('sample/hello_world.asm', 'Hello World')
+    ('./hello_world.asm', 'Hello World')
 ])
 def test_memory_to_str(asm_file, expected):
     executable = assemble(asm_file)
     mem = execute(executable)
     assert mem is not None
 
-    output = ''
+    actual = ''
     for index in range(1, len(expected) + 1):
-        byte = mem[- (4 * index): - (4 * index) + 4] if index != 1 else mem[- (4 * index):]
-        output += chr(int.from_bytes(byte, 'little', signed=False))
+        byte = mem[- (4 * index): - (4 * index) + 4] if index != 1 else mem[-4:]
+        actual += chr(int.from_bytes(byte, 'little', signed=False))
 
-    assert expected == output[::-1]
+    assert expected == actual[::-1]
