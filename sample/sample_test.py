6efb77f66b0b3709d539e1d87d0bcfb901beb7bf
Kern <gf4t47@gmail.com>
2018-09-13 00:19:31 -0700

    add space in "Hello World"

--- sample/sample_test.py
+++ sample/sample_test.py
@@ -4,19 +4,17 @@ from src.assembler.assembler import assemble
 from src.os.os import execute
 
 
-@pytest.mark.parametrize("assemble_name", [
-    'sample/hello_world.asm'
+@pytest.mark.parametrize("assemble_name, expected", [
+    ('sample/hello_world.asm', 'Hello World')
 ])
-def test_hello_world(assemble_name):
+def test_memory_to_str(assemble_name, expected):
     executable = assemble(assemble_name)
     mem = execute(executable)
     assert mem is not None
 
-    expected = 'HelloWorld'
     output = ''
     for index in range(1, len(expected) + 1):
         byte = mem[- (4 * index): - (4 * index) + 4] if index != 1 else mem[- (4 * index):]
         output += chr(int.from_bytes(byte, 'little', signed=False))
 
-    print(output)
     assert expected == output[::-1]
