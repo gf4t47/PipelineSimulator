d95d581c61b9854e9344fcadb9a300c7466f84fd
Kern <gf4t47@gmail.com>
2018-09-13 02:19:46 -0700

    add descriptions and sample tests in readme.md

--- src/index.py
+++ src/index.py
@@ -2,4 +2,9 @@ from src.os.os import process
 
 
 def execute(executable_file: str)->bytearray:
+    """
+    execute the binary executable file
+    :param executable_file: file path
+    :return: memory after the execution
+    """
     return process(executable_file)
