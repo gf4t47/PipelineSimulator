7750ceb578d69df8e79be9eda51142ba98b156fb
Kern <gf4t47@gmail.com>
2018-09-14 10:50:59 -0700

    re-order

--- src/util/converter.py
+++ src/util/converter.py
@@ -6,9 +6,9 @@ def inst_to_bytes(inst: int) -> bytes:
     return inst.to_bytes(4, 'little', signed=False)
 
 
-def imme_to_bytes(imme: int)->bytes:
-    return imme.to_bytes(2, 'little', signed=True)
-
-
 def imme_to_int(imme: bytearray) -> int:
     return int.from_bytes(imme, 'little', signed=True)
+
+
+def imme_to_bytes(imme: int)->bytes:
+    return imme.to_bytes(2, 'little', signed=True)
