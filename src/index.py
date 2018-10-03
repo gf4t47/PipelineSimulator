7f77e0867ffb5739e149ca29152d3605cfdb4db4
Kern <gf4t47@gmail.com>
2018-10-03 14:23:00 -0700

    update docs

--- src/index.py
+++ src/index.py
@@ -1,8 +1,11 @@
 # noinspection PyUnusedLocal
+from src.os.os import process
+
+
 def execute(executable_file: str)->bytearray:
     """
     execute the binary executable file
     :param executable_file: file path
     :return: memory after the execution
     """
-    pass
+    return process(executable_file)
